a0ea147a30d004c8afc1c68e796e6945fe5ba234
levon31 <levonhambardzumyan01@gmail.com>
2023-10-04 17:51:47 +0400

    changed getStaticProps to getServerSideProps to deploy purpose

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,22 +1,22 @@
 import {ReactElement} from "react";
-import type { GetStaticProps } from 'next'
+import type { GetServerSideProps } from 'next'
 import axios from 'axios';
 import {Team, NextPageWithLayout} from 'helpers/types';
 import MainLayout from 'layouts/MainLayout';
 import styles from 'styles/Home.module.css';
 
-export const getStaticProps = (async () => {
+export const getServerSideProps = (async () => {
   // Fetch mock data from the API, for more information check pages/api/teams/index.ts file
   const res = await axios.get(`${process.env.REACT_APP_API_URL}/teams`);
 
   // Return the fetched data as props,
   // so this props will be available in MainLayout, check pages/_app.tsx getLayout second argument
   return { props: { data: res.data.data } };
-}) satisfies GetStaticProps<{
+}) satisfies GetServerSideProps<{
   data: Team
 }>
 
-const Home: NextPageWithLayout<typeof getStaticProps> = () => {
+const Home: NextPageWithLayout<typeof getServerSideProps> = () => {
   return (
     <div className={styles.container}>Please Choose a team</div>
   );
